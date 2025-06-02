b0d7129da27188334d2f3583fa967b35ade2dbd7
restuchan <restu.chan188@gmail.com>
2025-06-02 14:23:56 +0700

    menambahkan virtual function/method pesan

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -3,4 +3,9 @@ using namespace std;
 
 class seseorang{
    public:
-       
+        //virtual void pesan() = 0;
+        virtual void pesan(){
+            cout << "Pesan dari seseorang" << endl;
+        }
+};
+
